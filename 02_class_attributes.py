bef18ba715f423c4759a81724eb145212f8d3e53
Robert Vari <mail.robertvari@gmail.com>
2023-07-29 14:20:19 +0200

    update

---
+++ 02_class_attributes.py
@@ -0,0 +1,11 @@
+class Dice:
+    sides = 6
+    color = "white"
+
+class Person:
+    name = "Kiss Csaba"
+    email = "csaba@gmail.com"
+    address = "Pécs"
+
+print(Dice.sides, Dice.color)
+print(Person.name, Person.email, Person.address)
